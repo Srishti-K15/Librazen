d84e8b45e2cdb92ce03ad85faaf2156f9dafe587
Srishti-K15 <srishtikjaiswal@gmail.com>
2024-06-13 12:57:23 +0530

    footer update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import './Footer.css'
+import logo from '../../images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faXTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      
-        <div className="footer-logo">
+      <div className='footer-container'>
+      <div className="footer-section left">
+        <div className='logo'>
         <h1 style={{fontFamily:"cursive"}}>Librazen</h1>
-      </div>
-      <div className="footer-bottom">
+        <img src={logo} alt="Librazen Logo" className='logo-icon' />
+        </div>
+        <div className="footer-copyright">
         <p>&copy; 2024 Librazen. All rights reserved.</p>
       </div>
+      </div>
       
+        <div className="footer-section social-media">
+          <ul>
+            <li><a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} alt="Facebook" /></a></li>
+            <li><a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faXTwitter} alt="Twitter" /></a></li>
+            <li><a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} alt="Instagram" /></a></li>
+            <li><a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} alt="LinkedIn" /></a></li>
+          </ul>
+        </div>
+        <div className="footer-section contact">
+          <p>Contact us: 
+          <a href="mailto:support@librazen.com">support@librazen.com</a></p>
+        </div>
+      </div>
     </footer>
   )
 }
